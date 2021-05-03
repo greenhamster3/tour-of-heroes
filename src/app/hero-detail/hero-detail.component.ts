55f3afbe452a26547d5d371af510bee232124980
greenhamster3 <greenhamster3incaps@gmail.com>
2021-05-03 12:02:14 +0100

    added hero detail components

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-
+  @Input() hero?: Hero;
   constructor() { }
 
   ngOnInit(): void {
