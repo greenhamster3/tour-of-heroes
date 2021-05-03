ecf7ecf5e7074e4d338cba3d3c3241fe3880edc4
greenhamster3 <greenhamster3incaps@gmail.com>
2021-05-03 16:26:24 +0100

    hacked list

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,15 +1,15 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 1, name: 'Robin'},
-  { id: 2, name: 'Starfire' },
-  { id: 3, name: 'Raven' },
-  { id: 4, name: 'Beast Boy' },
-  { id: 5, name: 'Cyborg' },
-  { id: 6, name: 'Super Man' },
-  { id: 7, name: 'Bat Man' },
-  { id: 8, name: 'Wonder Woman' },
-  { id: 9, name: 'Flash' },
-  { id: 10, name: 'Green Lantern' },
-  { id: 11, name: 'Martian Manhunter' }
+  { id: 1,  name: 'Starter name 1'},
+  { id: 2,  name: 'Starter name 2' },
+  { id: 3,  name: 'Starter name 3' },
+  { id: 4,  name: 'Starter name 4' },
+  { id: 5,  name: 'Starter name 5' },
+  { id: 6,  name: 'Starter name 6' },
+  { id: 7,  name: 'Starter name 7' },
+  { id: 8,  name: 'Starter name 8' },
+  { id: 9,  name: 'Starter name 9' },
+  { id: 10, name: 'Starter name 10' },
+  { id: 11, name: 'Starter name 11' }
 ];
